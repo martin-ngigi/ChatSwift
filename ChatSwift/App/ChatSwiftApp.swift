8e1d8bf674c9dc22dff811299b63a4b97a5f9b41
martin-ngigi <martinwainaina001@gmail.com>
2024-01-11 10:15:56 +0300

    Initial commit

---
+++ ChatSwift/App/ChatSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatSwiftApp.swift
+//  ChatSwift
+//
+//  Created by Martin Wainaina on 11/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
