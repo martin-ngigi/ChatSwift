660127b84a47170a9192ff8bc370c08a3c847fae
martin-ngigi <martinwainaina001@gmail.com>
2024-01-12 10:17:11 +0300

    Chat UI - Profile Pic, Message Input, Send Btn

---
+++ ChatSwift/Core/Chat/ChatView.swift
@@ -0,0 +1,69 @@
+//
+//  ChatView.swift
+//  ChatSwift
+//
+//  Created by Martin Wainaina on 12/01/2024.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    
+    @State private var messageText = ""
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                //header
+                VStack{
+                    CircularImageProfileView(user: User.MOCK_USER, size: .xLarge)
+                    
+                    VStack (spacing: 4) {
+                        
+                        Text("Martin Wainaina")
+                            .font(.title3)
+                        .fontWeight(.semibold)
+                        
+                        Text("Messenger")
+                            .font(.footnote)
+                            .foregroundStyle(.gray)
+                    }
+                    
+
+                }
+                
+                // messages
+                
+            }
+            
+            // message input view
+            Spacer()
+            
+            ZStack (alignment: .trailing){
+                
+                TextField("Type message ... ", text: $messageText, axis: .vertical)
+                    .padding(12)
+                    .padding(.trailing, 48)
+                    .background(Color(.secondarySystemBackground))
+                    .clipShape(Capsule())
+                    .font(.subheadline)
+                
+                Button {
+                    print("DEBUG: Send Button pressed")
+                } label: {
+                    Image(systemName: "paperplane.fill")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .scaledToFill()
+                        .foregroundColor(.gray)
+                        .padding()
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ChatView()
+}
