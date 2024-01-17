b875222675e3e918a9f05df2101b5c6c1c52211a
martin-ngigi <martinwainaina001@gmail.com>
2024-01-17 13:06:28 +0300

    Upload/Download User Data to and From firestore

--- ChatSwift/Core/Authentication/Service/AuthService.swift
+++ ChatSwift/Core/Authentication/Service/AuthService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Firebase
+import FirebaseFirestoreSwift
 
 class AuthService {
     
@@ -16,9 +17,11 @@ class AuthService {
     
     init() {
         self.userSession = Auth.auth().currentUser
+        Task { try await UserService.shared.fetchCurrentUser()}
         print("DEBUG: User session id is \(userSession?.uid)")
     }
     
+    @MainActor // pass thread from background thread to main thread
     func login(withEmail email: String, password: String) async throws {
         do {
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
@@ -29,10 +32,12 @@ class AuthService {
         }
     }
     
+    @MainActor
     func createUser(withEmail email: String, password: String, fullname: String) async throws {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
+            try await self.uploadUserData(email: email, fullname: fullname, id: result.user.uid)
             print("DEBUG: Create user \(result.user.uid)")
         }
         catch {
@@ -40,6 +45,7 @@ class AuthService {
         }
     }
     
+    @MainActor
     func signOut() {
         do {
             try Auth.auth().signOut() // signout from firebase
@@ -48,5 +54,11 @@ class AuthService {
         catch {
             print("DEBUG: failed to signout with error \(error.localizedDescription)")
         }
+    }
+    
+    private func uploadUserData(email: String, fullname: String, id: String) async throws{
+        let user = User(fullname: fullname, email: email, profileImageUrl: nil)
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return } // import FirebaseFirestoreSwift
+        try await Firestore.firestore().collection("users").document(id).setData(encodedUser)
     }
 }
