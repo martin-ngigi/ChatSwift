692c23b9c11c2f5d2a09c72eaf9637e9914b7cec
martin-ngigi <martinwainaina001@gmail.com>
2024-01-18 18:04:20 +0300

    Added AnimatedGradientBackround

---
+++ ChatSwift/Core/Authentication/View/AnimatedGradientBackround.swift
@@ -0,0 +1,42 @@
+//
+//  AnimatedGradientBackround.swift
+//  ChatSwift
+//
+//  Created by Martin Wainaina on 18/01/2024.
+//
+
+import SwiftUI
+
+struct AnimatedGradientBackround: View {
+    
+    let colors : [Color] = [.red, .blue, .green]
+    @State var animatedGradient: Bool = false
+    
+    var body: some View {
+        ZStack{
+            LinearGradient(
+                colors: colors,
+                startPoint: .topLeading,
+                endPoint: .bottom
+            )
+            .hueRotation(.degrees(animatedGradient ? 100 : 0))
+            .onAppear{
+                withAnimation (
+                    .easeInOut(duration: 3)
+                    .repeatForever(autoreverses: true)){
+                        animatedGradient.toggle()
+                    }
+            }
+            
+            Color
+                .clear
+                .background(.thinMaterial)
+            
+        
+        }
+    }
+}
+
+#Preview {
+    AnimatedGradientBackround()
+}
